127015f763524ea4cb8fdc0055eaf0685636ebc5
xuanmanhdao <xuanmanhdao2001@gmail.com>
2022-11-06 20:50:05 +0700

    Done base for Quan Ly Kho

---
+++ resources/views/QuanLyKho/layout/header.blade.php
@@ -0,0 +1,26 @@
+<div class="header d-flex justify-content-between align-items-center">
+    <div class="logo">
+        <a href="#">
+            <img src="{{ asset('img/quanlykho/login_logo.png') }}" alt="">
+        </a>
+    </div>
+    <div class="menu d-flex justify-content-between align-items-center">
+        <ul>
+            <li><a href="#">TRANG CHỦ</a></li>
+            <li><a href="#">HỎI ĐÁP</a></li>
+            <li><a href="#">LIÊN HỆ</a></li>
+        </ul>
+        <ul class="menu2">
+            <li>
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </li>
+            <li class="menu2-avatar">
+                <img src="{{ asset('img/quanlykho/avatar.png') }}" alt="">
+                <i class="fa-solid fa-caret-down"></i>
+            </li>
+            <li>
+                <p>VIE</p>
+            </li>
+        </ul>
+    </div>
+</div>
