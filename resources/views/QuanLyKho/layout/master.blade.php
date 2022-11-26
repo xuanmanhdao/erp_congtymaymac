20d14dcb4cae03989ef32182850b70506df36b15
xuanmanhdao <xuanmanhdao2001@gmail.com>
2022-11-27 01:57:12 +0700

    Change Sidebar interface in QuanLyKho

--- resources/views/QuanLyKho/layout/master.blade.php
+++ resources/views/QuanLyKho/layout/master.blade.php
@@ -14,6 +14,9 @@
     <link href="{{ asset('css/quanlykho/product.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+{{-- 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
     @stack('css')
 </head>
 
@@ -74,9 +77,20 @@
     <script src="{{ asset('js/quanlykho/main.js') }}"></script>
 
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    
+    {{-- bootstrap submenu --}}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     @stack('js')
 </body>
 
--- resources/views/QuanLyKho/layout/sidebar.blade.php
+++ resources/views/QuanLyKho/layout/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="nav">
     <ul>
-        <li class="nav-items nav-active">
+        {{-- <li class="nav-items nav-active">
             <a href="{{ route('loaisanpham.index') }}">
                 Quản lý loại sản phẩm
             </a>
@@ -34,7 +34,8 @@
             <a href="{{ route('nguyenvatlieu.index') }}">
                 Quản lý nguyên vật liệu tồn kho
             </a>
-        </li>
+        </li>  --}}
+
         {{-- <li class="nav-items">
             <a href="#">
                 Xuất hàng
@@ -50,5 +51,79 @@
                 Báo cáo
             </a>
         </li> --}}
+
+
+        <li class="nav-items dropright">
+            <a href="javascript:void(0)" class="dropdown-toggle" type="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false" style="white-space: inherit !important;">
+                Quản lý kho nguyên vật liệu
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a class="sub-menu-content" href="{{ route('donviphanphoi.index') }}">
+                        Quản lý đơn vị cung cấp
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown-divider"></div>
+                </li>
+                <li>
+                    <a class="sub-menu-content" href="{{ route('loainguyenvatlieu.index') }}">
+                        Quản lý loại nguyên vật liệu
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown-divider"></div>
+                </li>
+                <li>
+                    <a class="sub-menu-content" href="{{ route('nhapnguyenlieu.index') }}">
+                        Quản lý nhập nguyên vật liệu
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown-divider"></div>
+                </li>
+                <li>
+                    <a class="sub-menu-content" href="{{ route('nguyenvatlieu.index') }}">
+                        Quản lý nguyên vật liệu tồn kho
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-items dropright">
+            <a href="javascript:void(0)" class="dropdown-toggle" type="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false" style="white-space: inherit !important;">
+                Quản lý kho sản phẩm
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a class="sub-menu-content" href="{{ route('loaisanpham.index') }}">
+                        Quản lý loại sản phẩm
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown-divider"></div>
+                </li>
+                <li>
+                    <a class="sub-menu-content" href="{{ route('nhapsanpham.index') }}">
+                        Quản lý nhập sản phẩm
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown-divider"></div>
+                </li>
+                <li>
+                    <a class="sub-menu-content" href="{{ route('sanpham.index') }}">
+                        Quản lý sản phẩm tồn kho
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="nav-items">
+            <a href="#">
+                Báo cáo
+            </a>
+        </li>
     </ul>
 </div>
